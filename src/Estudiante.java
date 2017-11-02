54fbb53dc4043b515b676d71f37ccfbe646bdb8b
Unknown <sil17276@uvg.edu.gt>
2017-11-02 08:46:08 -0600

    Añadiendo herencia

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -3,13 +3,65 @@
  */
 
 /**
- * @author smayr
+ * @author Mayra Silva 17276
+ * Oliver Graf 17190
  *
  */
 public class Estudiante {
-protected nombre String; 
-protected dpi String;
-protected notaFinal float;
+	protected String nombre;
+	protected String dpi;
+	protected float notaFinal;
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the dpi
+	 */
+	public String getDpi() {
+		return dpi;
+	}
+	/**
+	 * @param dpi the dpi to set
+	 */
+	public void setDpi(String dpi) {
+		this.dpi = dpi;
+	}
+	/**
+	 * @return the notaFinal
+	 */
+	public float getNotaFinal() {
+		return notaFinal;
+	}
+	/**
+	 * @param notaFinal the notaFinal to set
+	 */
+	public void setNotaFinal(float notaFinal) {
+		this.notaFinal = notaFinal;
+	}
+	
+	/**Constructor
+	 * @param nombre
+	 * @param dpi
+	 * @param notaFinal
+	 */
+	public Estudiante(String nombre, String dpi, float notaFinal) {
+		super();
+		this.nombre = nombre;
+		this.dpi = dpi;
+		this.notaFinal = notaFinal;
+	}
+	public Estudiante() {
+	}
+	
+	}
 
 
-}
---
+++ src/Secundaria.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author smayr
+ *
+ */
+public class Secundaria extends Estudiante{
+	protected float promSeptimo;
+	protected float promOctavo;
+	protected float promNoveno;
+	protected float notaMate;
+	protected float notaHistori;
+	protected float notaEspanol;
+	
+	
+	public Secundaria() {
+		super();
+		
+	}
+
+}
